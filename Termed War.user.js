f379af5a168a999acbdcda6b34dc1f62229c2c02
sid-the-sloth1 <blahblahbrian@protonmail.com>
2020-10-17 15:01:53 +0530

    testing the new button

--- Termed War.user.js
+++ Termed War.user.js
@@ -6,48 +6,68 @@
 // @author       You
 // @match        https://www.torn.com/loader.php?sid=attack*
 // ==/UserScript==
- 
+
 (function() {
-    'use strict';
- 
-    let mug = false;
-    let hosp = false;
-    let leave = true;
-    let special = true;
- 
-    function changeCSS() {
-        let optionsBox = document.querySelector('div[class^="dialogButtons"]').children;
-        for (const option of optionsBox) {
-            let text = option.innerText.toLowerCase();
-            if (!mug && text === "mug") {
-                option.style.display = "none";
-            } else if (!hosp && text === "hosp") {
-                option.style.display = "none";
-            } else if (!leave && text === "leave") {
-                option.style.display = "none";
-            }
-        }
+  'use strict';
+
+  let mug = false;
+  let hosp = false;
+  let leave = true;
+
+  let storage = {};
+
+  const config = {attributes: true, childList: true, subtree: true };
+
+  const observer = new MutationObserver((mutations) => {
+    for (const mutation of mutations) {
+      if (document.querySelectorAll('div[class^="dialogButtons"]').length > 0) {
+        changeCSS();
+      }
     }
-    const config = {attributes: true, childList: true, subtree: true };
- 
-    const observer = new MutationObserver((mutations) => {
-        for (const mutation of mutations) {
-            if (document.querySelectorAll('div[class^="dialogButtons"]').length > 0) {
-                changeCSS();
-            }
-        }
-    })
-    var cd = setInterval(checkForElement,1000);
-    function checkForElement() {
-        if (document.querySelectorAll('div[class^="playerArea"]').length > 0) {
-            clearInterval(cd);
-            const wrapper = document.querySelector('div[class^="playerArea"]');
-            observer.observe(wrapper, config);
-        }
+  })
+  var cd = setInterval(checkForElement,
+    1000);
+  function checkForElement() {
+    if (document.querySelectorAll('div[class^="playerArea"]').length > 0) {
+      clearInterval(cd);
+      const wrapper = document.querySelector('div[class^="playerArea"]');
+      observer.observe(wrapper, config);
     }
-    //const wrapper = document.querySelector('div[class^="playerArea"]');
-    //const wrapper = document.body;
- 
- 
+  }
+
+  function disconnect() {
+    if (!storage.index) {
+      observer.disconnect();
+      storage.index = 69;
+    }
+  }
+  //const wrapper = document.querySelector('div[class^="playerArea"]');
+  //const wrapper = document.body;
+  function changeCSS() {
+    let optionsDialogBox = document.querySelector('div[class^="dialogButtons"]');
+    let optionsBox = optionsDialogBox.children;
+    for (const option of optionsBox) {
+      let text = option.innerText.toLowerCase();
+      if (!mug && text === "mug") {
+        option.style.display = "none"
+        disconnect();
+      } else if (!hosp && text === "hosp") {
+        option.style.display = "none";
+        disconnect();
+      } else if (!leave && text === "leave") {
+        option.style.display = "none";
+        disconnect();
+      }
+
+      if (!mug || !hosp || !leave) {
+        if (text == "leave") {
+          let showOptionsButton = document.createElement("button");
+          showOptionsButton.className = option.className;
+          showOptionsButton.innerText = "Show All Options";
+          optionsDialogBox.appendChild(showOptionsButton);
+        }
+      }
+    }
+  }
+
 })();
- 
