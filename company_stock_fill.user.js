39361738ebca8409717246d24499dade3ce98b9e
Hardy <blahblahbrian@protonmail.com>
2021-02-15 09:53:38 +0530

    Test mobile mode

--- company_stock_fill.user.js
+++ company_stock_fill.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Company Stock Order
 // @namespace    hardy.company.stock
-// @version      1.0
+// @version      1.1
 // @description  Auto-fills Company Stock
 // @author       Hardy [2131687]
 // @match        https://www.torn.com/companies.php*
@@ -9,20 +9,24 @@
 // ==/UserScript==
 
 (function() {
-    'use strict';
-    var cooldown = setInterval(fillStock, 1500);
-    function fillStock() {
-        let form = document.querySelector("form[action^='companies.php?step=stock1']");
-        if (form) {
-            clearInterval(cooldown);
-            let nodeList = document.querySelector(".stock-list-wrap .stock-list").children;
-            for (const node of nodeList) {
-                if (!node.className) {
-                    let amount = node.querySelector(".info-wrap .sold-daily").innerText;
-                    console.log(amount);
-                    node.querySelector(".quantity input").value = amount;
-                }
-            }
+  'use strict';
+  var cooldown = setInterval(fillStock, 1500);
+  function fillStock() {
+    let form = document.querySelector("form[action^='companies.php?step=stock1']");
+    if (form) {
+      clearInterval(cooldown);
+      let nodeList = document.querySelector(".stock-list-wrap .stock-list").children;
+      for (const node of nodeList) {
+        if (!node.className) {
+          let obj = {};
+          obj.amount = node.querySelector(".info-wrap .sold-daily").innerText;
+          if (obj.amount.toUpperCase().includes("SOLD")) {
+            obj.amount = obj.amount.replace(/\s/g, "").replace(/"/g, "").split(":")[1]
+          }
+          //console.log(amount);
+          node.querySelector(".quantity input").value = obj.amount;
         }
+      }
     }
+  }
 })();
